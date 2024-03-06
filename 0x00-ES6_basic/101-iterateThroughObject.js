4a947b976fd7b47f1591c5836d4cd35dd09e8f5e
Alexander Iwuchukwu <iwuchukwualexander@gmail.com>
2024-03-06 06:36:10 +0000

    ES6_basic files

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const iterator = reportWithIterator;
-  let result = '';
-
-  let next = iterator.next();
-  while (!next.done) {
-    result += next.value;
-    next = iterator.next();
-    if (!next.done) {
-      result += ' | ';
-    }
-  }
-
-  return result;
+  return reportWithIterator.join(' | ');
 }
